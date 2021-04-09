8ebcf26787d985c9a5c1bd0f339b6458179801df
blue928 <bluepresley@gastonia.com>
2021-04-09 14:07:50 -0400

    Checking for tmpfile

--- autoinstallwordpresscpanel.sh
+++ autoinstallwordpresscpanel.sh
@@ -5,8 +5,8 @@
 
 TMPFILE="$(mktemp -p /tmp wp-auto-install-XXXXXXXX)"
 cat "${1:-/dev/stdin}" > $TMPFILE
-DOMAIN=$(python -c "import sys, json; print json.load(open('$TMPFILE'))['data']['domain']")
-CUSTOMEREMAIL=$(python -c "import sys, json; print json.load(open('$TMPFILE'))['data']['contactemail']")
+DOMAIN=$(python3 -c "import sys, json; print json.load(open('$TMPFILE'))['data']['domain']")
+CUSTOMEREMAIL=$(python3 -c "import sys, json; print json.load(open('$TMPFILE'))['data']['contactemail']")
 
 ## The following variable contains the name of the Package that was assigned to the account.
 ## It would be possible to add additinal logic to this script so that WordPress is only installed for certain packages
@@ -14,4 +14,15 @@ CUSTOMEREMAIL=$(python -c "import sys, json; print json.load(open('$TMPFILE'))['
 
 rm -f $TMPFILE
 
-wp-toolkit --install -domain-name $DOMAIN -admin-email $CUSTOMEREMAIL
+#Softaculous
+#/usr/local/cpanel/3rdparty/bin/php /usr/local/cpanel/whostmgr/docroot/cgi/softaculous/cli.php \
+#    --install \
+#    --panel_user=USER \
+#    --panel_pass=PASS \
+#    --soft=26 \
+#    --softdirectory=test \
+#    --admin_username=admin \
+#    --admin_pass=pass
+
+#Wp Toolkit
+#wp-toolkit --install -domain-name $DOMAIN -admin-email $CUSTOMEREMAIL
